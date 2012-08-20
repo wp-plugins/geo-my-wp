f3dc97a79402daac741ba73d872f9bde1ce1917d
ninjew <ninjew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-20 17:11:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/geo-my-wp/tags/1.5.1@587879 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/toggle.js
@@ -0,0 +1,8 @@
+jQuery(function() {
+	//// show hide map ///
+    jQuery('.wppl-help-btn').click(function(event){
+    	event.preventDefault();
+    	jQuery(this).closest("div").find(".wppl-help-message").slideToggle();
+    
+    }); 
+});
