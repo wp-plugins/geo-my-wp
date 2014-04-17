df8f14ab37e9fc495bdaa6b6358b93aa8e3b6942
ninjew <ninjew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-17 05:21:06 +0000

    Update 2.4.2.1
    git-svn-id: https://plugins.svn.wordpress.org/geo-my-wp/tags/2.4.2.1@895625 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins/friends/search-forms/default/search-form.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Default search form for Buddypress members.
+ * @version 1.0
+ * @author Eyal Fitoussi
+ */
+?>
+<div class="gmw-form-wrapper gmw-form-wrapper-<? echo $gmw['ID']; ?> gmw-fl-form-wrapper">
+
+    <form class="standard-form gmw-form gmw-form-<? echo $gmw['ID']; ?>" name="gmw_form" action="<?php echo $gmw[ 'search_results' ][ 'results_page' ]; ?>" method="get">
+
+        <?php do_action( 'gmw_search_form_start', $gmw ); ?>
+
+        <?php gmw_fl_xprofile_fields( $gmw, $class='' ); ?>
+
+        <?php do_action( 'gmw_search_form_before_address', $gmw ); ?>
+
+        <!-- Address Field -->
+		<?php gmw_search_form_address_field( $gmw, $id='', $class='' ); ?>
+	
+		<?php do_action( 'gmw_search_form_before_locator', $gmw ); ?>
+		
+        <!--  locator icon -->
+        <?php gmw_search_form_locator_icon( $gmw, $class='' ); ?>
+
+        <div class="clear"></div>	
+
+        <?php do_action( 'gmw_search_form_before_distance', $gmw ); ?>
+
+        <div class="gmw-unit-distance-wrapper">
+            
+            <div class="gmw-radius-dropdown-wrapper">
+            	<!--distance values -->
+            	<?php gmw_search_form_radius_values( $gmw, $class='' ); ?>
+            </div>
+            
+            <div class="gmw-units-dropdown-wrapper">
+            	<!--distance units-->
+            	<?php gmw_search_form_units( $gmw, $class='' ); ?>
+            </div>	
+        </div>
+
+        <?php gmw_form_submit_fields( $gmw, __( 'Submit', 'GMW' ) ); ?>
+
+        <?php do_action( 'gmw_search_form_end', $gmw ); ?>
+
+    </form>
+
+</div><!--form wrapper -->	
