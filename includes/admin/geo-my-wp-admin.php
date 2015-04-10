719e100e03b6c735c03856666cd103fac6290b56
ninjew <ninjew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 09:24:53 +0000

    New version
    git-svn-id: https://plugins.svn.wordpress.org/geo-my-wp/tags/2.6.0.1@1131899 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin/geo-my-wp-admin.php
@@ -0,0 +1,352 @@
+<?php
+if (!defined('ABSPATH'))
+	exit; // Exit if accessed directly
+
+/**
+ * GMW_Admin class.
+ */
+class GMW_Admin {
+
+	/**
+	 * __construct function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+			
+		//get options
+		$this->addons   = get_option('gmw_addons');
+		$this->settings = get_option('gmw_options');
+			
+		//do some actions	
+		add_action( 'admin_init', 			 array( $this, 'init_addons' ) );
+		add_action( 'admin_menu', 			 array( $this, 'admin_menu' ), 12);
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'media_buttons',  		 array( $this, 'add_form_button'), 25 );
+		add_action( 'admin_footer',  		 array( $this, 'form_insert_popup') );
+		
+		//include admin pages
+		include_once( 'geo-my-wp-admin-functions.php' );
+		include_once( 'tools/geo-my-wp-tools.php' );
+		include_once( 'geo-my-wp-addons.php' );
+		include_once( 'geo-my-wp-settings.php' );
+		include_once( 'geo-my-wp-forms.php' );
+		include_once( 'geo-my-wp-edit-form.php' );
+		include_once( 'geo-my-wp-shortcodes.php' );
+		
+		//set pages
+		$this->addons_page     	= new GMW_Addons();
+		$this->settings_page   	= new GMW_Settings();
+		$this->forms_page      	= new GMW_Forms();
+		$this->edit_form_page 	= new GMW_Edit_Form;
+		$this->shortcodes_page 	= new GMW_Shortcodes_page();
+	
+		add_filter( 'plugin_action_links_geo-my-wp/geo-my-wp.php', array( $this, 'gmw_action_links' ), 10, 2 );
+		
+		//for loer version of plugin
+		add_filter( 'plugin_action_links', array( $this, 'addons_action_links' ), 10, 2 );
+		
+		//display footer credits only on GEO my WP pages
+		$gmw_pages = array( 'gmw-add-ons', 'gmw-settings', 'gmw-forms', 'gmw-shortcodes', 'gmw-tools' );
+		
+		if ( isset( $_GET['page'] ) && in_array( $_GET['page'], $gmw_pages ) ) {
+			add_filter( 'admin_footer_text', array( $this, 'gmw_credit_footer'), 10 );
+		}
+		
+		add_action( 'form_editor_tab_end', array( $this, 'rickey_messick_credit' ), 10, 4 );
+	}
+	
+	/**
+	 * admin_enqueue_scripts function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function admin_enqueue_scripts() {
+		
+		$googleApi = ( !empty( $this->settings['general_settings']['google_api'] ) ) 	? '&key=' . $this->settings['general_settings']['google_api'] : '';
+		$region	   = ( !empty( $this->settings['general_settings']['country_code'] ) )  ? '&region=' .$this->settings['general_settings']['country_code'] : '';
+		$language  = ( !empty( $this->settings['general_settings']['language_code'] ) ) ? '&language=' .$this->settings['general_settings']['language_code'] : '';
+	
+		//wp_enqueue_script( 'jquery-ui-sortable' );
+		
+		//register google maps api
+		wp_register_script( 'google-maps', ( is_ssl() ? 'https' : 'http' ) . '://maps.googleapis.com/maps/api/js?libraries=places'.$googleApi.'&sensor=false'.$region.$language, array( 'jquery' ), false );
+		
+		wp_register_style( 'gmw-style-admin', GMW_URL . '/includes/admin/assets/css/style-admin.css' );
+		wp_enqueue_style( 'gmw-style-admin' );
+		
+		wp_register_script( 'gmw-admin', GMW_URL.'/includes/admin/assets/js/gmw-admin.js', array( 'jquery' ), GMW_VERSION, true );
+        wp_enqueue_script( 'gmw-admin' ); 
+
+        //wp_register_script( 'chosen', GMW_URL . '/assets/js/chosen.jquery.min.js', array( 'jquery' ), GMW_VERSION, true );
+        //wp_register_style( 'chosen',  GMW_URL . '/assets/css/chosen.min.css' );
+	}
+	
+	/**
+	 * admin_menu function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function admin_menu() {
+	
+		add_menu_page( 'GEO my WP', 'GEO my WP', 'manage_options', 'gmw-add-ons', array($this->addons_page, 'output' ), '', 66 );
+		add_submenu_page( 'gmw-add-ons', __( 'Add-ons', 'GMW' ), __( 'Add-ons', 'GMW' ), 'manage_options', 'gmw-add-ons', array( $this->addons_page, 'output' ) );
+		add_submenu_page( 'gmw-add-ons', __( 'GEO my WP Settings', 'GMW' ), __( 'Settings', 'GMW'), 'manage_options', 'gmw-settings', array( $this->settings_page, 'output' ) );
+		add_submenu_page( 'gmw-add-ons', __( 'Forms', 'GMW' ), __( 'Forms', 'GMW' ), 'manage_options', 'gmw-forms', ( !empty( $_GET['gmw_action']) && $_GET['gmw_action'] == 'edit_form' ) ? array( $this->edit_form_page, 'output' ) : array( $this->forms_page, 'output' ) );
+		add_submenu_page( 'gmw-add-ons', __( 'Tools', 'GMW' ), __( 'Tools', 'GMW' ), 'manage_options', 'gmw-tools', 'gmw_tools_page_output' );
+		add_submenu_page( 'gmw-add-ons', __( 'Shortcodes', 'GMW' ), __( 'Shortcodes', 'GMW' ), 'manage_options', 'gmw-shortcodes', array( $this->shortcodes_page, 'output' ) );
+	
+		$menu_items = array();
+	
+		//hook your add-on's menu item
+		$menu_items = apply_filters('gmw_admin_menu_items', $menu_items);
+	
+		foreach ( $menu_items as $item ) {
+			add_submenu_page('gmw-add-ons', $item['page_title'], $item['menu_title'], $item['capability'], $item['menu_slug'], $item['callback_function']);
+		}	
+	}
+	
+	/**
+	 * Initiate all GMW's add-ons
+	 *
+	 */
+	public function init_addons() {
+				
+		$addons_data   	   = array();
+		$new_addons_status = array();
+						
+		//hook your add-on here
+		$addons_data = apply_filters( 'gmw_admin_addons_page', $addons_data );
+
+		foreach ( $addons_data as $addon ) {	
+
+			if ( $addon['name'] == 'posts' && isset( $this->addons['posts'] ) && $this->addons['posts'] == 'active' ) {
+				$new_addons_status['posts'] = 'active';
+			} elseif ( $addon['name'] == 'friends' && isset( $this->addons['friends'] ) && $this->addons['friends'] == 'active' ) {
+				$new_addons_status['friends'] = 'active';
+			} elseif ( $addon['name'] != 'posts' && $addon['name'] != 'friends' ) { 
+				$new_addons_status[$addon['name']] = 'active';
+			}		
+		}
+		
+		//update addons data into database
+		update_option( 'gmw_addons', 	  $new_addons_status );
+		update_option( 'gmw_addons_data', $addons_data 		 );		
+		
+		//pass add-ons data to add-ons page
+		$this->addons_page->addons 	    = $new_addons_status;
+		$this->addons_page->addons_data = $addons_data;
+	}
+	
+	/**
+	 * add gmw action links in plugins page
+	 * @param $links
+	 * @param $file
+	 */
+	public function gmw_action_links( $links, $file ) {
+		
+		$links[] = '<a href="' . admin_url('admin.php?page=gmw-settings').'">' . __( 'Settings' , 'GMW') . '</a>';
+		
+		return $links;
+	}
+
+	public function addons_action_links( $links, $file ) {
+		static $this_plugin;
+	
+		$licenses = get_option('gmw_license_keys');
+		$statuses = get_option('gmw_premium_plugin_status');
+	
+		$deactivate_links = array();
+		$deactivate_links = apply_filters( 'gmw_plugin_action_links', $deactivate_links );
+	
+		if ( empty( $deactivate_links ) ) 
+			return $links;
+		
+		foreach ( $deactivate_links as $addon => $link ) {
+	
+			if ( $file == $link ) {
+	
+				if ( isset($this->addons[$addon]) && $this->addons[$addon] == 'active' && !empty( $licenses[$addon] ) && isset($statuses[$addon]) && $statuses[$addon] == 'valid' )
+					$links['deactivate'] = '<a href="' . admin_url( 'admin.php?page=gmw-add-ons' ).'">'.__( 'Please deactivate the license key before deactivating the plugin', 'GMW' ).'</a>';
+				else
+					$links = array( 'activate' => '<a href="'.admin_url( 'admin.php?page=gmw-add-ons' ).'">' . __('Activate license key', 'GMW') . '</a>') + $links;
+			}
+		}
+		return $links;
+	}
+	
+	/**
+	 * Action target that adds the "Insert Form" button to the post/page edit screen
+	 */
+    public static function add_form_button(){
+
+    	// do a version check for the new 3.5 UI
+        $version = get_bloginfo('version');
+
+        if ( $version < 3.5 ) {
+            // show button for v 3.4 and below
+            $image_btn = GFCommon::get_base_url() . "/images/form-button.png";
+            echo '<a href="#TB_inline?width=480&inlineId=select_gmw_form" class="thickbox" id="add_gmw_form" title="' . __("Add GEO my WP Form", 'GMW') . '"><img src="'.$image_btn.'" alt="' . __("GMW Form", 'GMW') . '" /></a>';
+        } else {
+            // display button matching new UI
+            echo '<style>
+            		.gmw_media_icon:before {
+            			content: "\f230" !important;
+						color: rgb(103, 199, 134) !important;
+					}
+            		.gmw_media_icon {
+                    	vertical-align: text-top;
+                    	width: 18px;
+                    }
+                    .wp-core-ui a.gmw_media_link{
+                     	padding-left: 0.4em;
+                    }
+                 </style>
+                 <a href="#TB_inline?width=480&inlineId=select_gmw_form" class="thickbox button gmw_media_link" id="add_gmw_form" title="' . __("Add GEO my WP Form", 'GMW') . '"><span class="gmw_media_icon dashicons"></span> ' . __("GMW Form", 'GMW') . '</a>';
+        }
+    }
+    
+    /**
+    * popup to inset GEO my WP form into content area
+    *
+    */
+    public static function form_insert_popup(){
+    	?>
+            <script>
+                function gmwInsertForm(){
+                                    	
+                    if ( jQuery('.gmw_form_type:checked').val() != 'results' ) { 
+                        
+                    	var form_id = jQuery("#gmw_form_id").val();
+                        if(form_id == ""){
+                            alert("<?php _e("Please select a form", "GMW") ?>");
+                            return;
+                        }
+                        
+                    	var form_name = jQuery("#gmw_form_id option[value='" + form_id + "']").text().replace(/[\[\]]/g, '');
+                    	window.send_to_editor("[gmw "+ jQuery('.gmw_form_type:checked').val() + "=\"" + form_id + "\" name=\"" + form_name + "\"]");
+                    	
+                    } else {
+                        
+                    	window.send_to_editor('[gmw form="results"]');
+                    }
+                }
+            </script>
+    
+            <div id="select_gmw_form" style="display:none;">
+            <div class="wrap">
+                <div>
+                    <div style="padding:15px 15px 0 15px;">
+                        <h3 style="color:#5A5A5A!important; font-family:Georgia,Times New Roman,Times,serif!important; font-size:1.8em!important; font-weight:normal!important;"><?php _e("Insert A Form Shortcode", "GMW"); ?></h3>
+                        <span>
+                            <?php _e("Select the type of shortcode you wish to add", "GMW"); ?>
+                        </span>
+                    </div>
+                    <div style="padding:15px 15px 0 15px;">
+                        <input type="radio" class="gmw_form_type" checked="checked" name="gmw_form_type" value="form" onclick="if ( jQuery('#gmw-forms-dropdown-wrapper').is(':hidden') ) jQuery('#gmw-forms-dropdown-wrapper').slideToggle();" /> <label for="gmw-form"><?php _e("Form shortcode", "GMW"); ?></label> &nbsp;&nbsp;&nbsp;
+                        <input type="radio" class="gmw_form_type" name="gmw_form_type"  value="map" onclick="if ( jQuery('#gmw-forms-dropdown-wrapper').is(':hidden') ) jQuery('#gmw-forms-dropdown-wrapper').slideToggle();" /> <label for="gmw-map"><?php _e("Map Shortcode", "GMW"); ?></label>&nbsp;&nbsp;&nbsp;
+                        <input type="radio" class="gmw_form_type" name="gmw_form_type" value="results" onclick="if ( jQuery('#gmw-forms-dropdown-wrapper').is(':visible') ) jQuery('#gmw-forms-dropdown-wrapper').slideToggle();" /> <label for="gmw-form"><?php _e("Results shortcode", "GMW"); ?></label> &nbsp;&nbsp;&nbsp;
+                    </div>
+                    <div id="gmw-forms-dropdown-wrapper" style="padding:15px 15px 0 15px;">
+                        <select id="gmw_form_id">
+                            <option value="">  <?php _e("Select a Form", "GMW"); ?>  </option>
+                            <?php
+                                $forms = get_option('gmw_forms');
+                                foreach( $forms as $form ) {
+                                	$form['name'] = ( !empty( $form['name'] ) ) ? $form['name'] : 'form_id_'.$form['ID'];
+                                    ?>
+                                    <option value="<?php echo absint( $form['ID'] ); ?>"><?php echo esc_html( $form['name'] ); ?></option>
+                                    <?php
+                                }
+                            ?>
+                        </select>
+                    </div>
+                   
+                    <div style="padding:15px;">
+                        <input type="button" class="button-primary" value="<?php _e("Insert Shortcode", "GMW"); ?>" onclick="gmwInsertForm();"/>&nbsp;&nbsp;&nbsp;
+                    	<a class="button" style="color:#bbb;" href="#" onclick="tb_remove(); return false;"><?php _e("Cancel", "GMW"); ?></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    <?php
+    }
+		
+    /**
+     * GEO my WP credits - header
+     * @return string
+     */
+	static public function gmw_credits() {
+
+		$output  =	'<div class="gmw-credits">';
+		$output .=	'<img src="'.GMW_URL .'/assets/images/gmw-logo.png" />';
+		$output .=          '<div style="display: inline-block;"><a href="http://www.geomywp.com" target="_blank">'.__( 'Developed by Eyal Fitoussi - please take a moment to support my work' ,'GJM').'</a></div>';
+		$output .=          '<div>';
+		$output .=          	'<a href="http://wordpress.org/plugins/geo-my-wp/" title="Rate GEO my WP" target="_blank"><img src="'.GMW_URL .'/assets/images/star-icon.png" style="max-height:23px;" /></a>';
+		$output .= 				'<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WTF4HGEYNFF8W" class="gmw-credits-addons-button" target="_blank" Title="Thank you">Donate</a>';
+		$output .=              '<a class="gmw-credits-facebook-button" title="GEO my WP Facebook page" href="https://www.facebook.com/geomywp" target="_blank">Facebook</a>';
+		$output .=              '<a class="gmw-credits-twitter-button" title="GEO my WP Twitter page" href="https://twitter.com/GEOmyWP" target="_blank">Twitter</a>';
+		$output .=              '<a class="gmw-credits-email-button" title="Contact Us" href="mailto:info@geomywp.com" title="Email" target="_blank">Email</a>';
+		$output .=              '<div style="float:left;margin-top: 2px;" class="fb-like" data-href="https://www.facebook.com/geomywp" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>';
+		$output .=              '<span style="margin: 2px 4px 5px 4px;float:left;"><a href="https://twitter.com/GEOmyWP" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false" style="margin-top:2px">Follow @GEOmyWP</a></span>';
+		$output .=              '<a class="gmw-credits-addons-button" title="GEO my WP Add-ons" href="http://geomywp.com/add-ons" target="_blank">Add-ons</a>';
+		$output .=          '</div>';
+		$output .=	'</div>';
+		?>
+		<div id="fb-root"></div>
+		<script>
+            !function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = p + '://platform.twitter.com/widgets.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }
+            }(document, 'script', 'twitter-wjs');
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id))
+                    return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=150962325088686";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+		<?php
+		return $output;
+
+	}
+	
+	/**
+	 * GMW credit footer
+	 * @param unknown_type $content
+	 * @return string
+	 */
+	static public function gmw_credit_footer( $content ) {
+		return preg_replace('/[.,]/', '', $content) . ' ' . __( 'and Geo-locating with', 'GMW' ). ' <a href="http://geomywp.com" target="_blank" title="GEO my WP">'.__( 'GEO my WP', 'GMW' ) . '</a>.';	
+	}
+	
+	public function rickey_messick_credit( $key, $section, $formID, $gmw ) {
+	
+		if ( $key != 'page_load_results' || $gmw['prefix'] != 'pt' ) 
+			return;
+		
+		?>
+		<tr class="gmw-sponsored-credit">
+			<td></td>
+			<td>
+				<span>This tab and features were sponsored by <a href="http://www.rickeymessick.com" target="_blank" title="Rickey Messick Credit">Rickey Messick</a>. Thank you!</span>
+			</td>
+			<td></td>
+		</tr>
+		<?php 		
+	}
+}
+new GMW_Admin();
+?>
---
+++ includes/geo-my-wp-shortcodes.php
@@ -0,0 +1,231 @@
+<?php
+
+/**
+ * User's current location class
+ *
+ * @version 1.0
+ * @author Eyal Fitoussi
+ */
+class GMW_Current_location {
+
+    /**
+     * __constructor
+     */
+    public function __construct() {
+
+        add_shortcode( 'gmw_current_location', array($this, 'current_location' ) );
+        add_action( 'wp_enqueue_scripts', 	   array($this, 'register_scripts_frontend' ) );
+
+        if ( !has_action( 'wp_footer', array( $this, 'cl_template' ) ) ) {
+            add_action( 'wp_footer', array( $this, 'cl_template' ) );
+        }
+        add_action( 'init', array( $this, 'submitted_location' ) );
+
+    }
+
+    /**
+     * Register scripts
+     */
+    public function register_scripts_frontend() {
+        wp_register_script( 'gmw-cl-js', GMW_URL . '/assets/js/gmw-cl.min.js', array('jquery'), GMW_VERSION, true );
+    }
+
+    /**
+     * Get current location
+     * @param $args
+     */
+    public function current_location( $org_args ) {
+
+    	$args = shortcode_atts( array(
+    			'scid'		 			=> rand( 1,100 ),
+    			'title'      			=> 'Your location',
+    			'display_by' 			=> 'city,country',
+    			'text_only'	 			=> 0,
+    			'show_name'  			=> 1,
+    			'user_message' 			=> 'Hello',
+    			'guest_message' 		=> 'Hello, guest!',
+    			'map'		 			=> 1,
+    			'map_height' 			=> '200px',
+    			'map_width'  			=> '200px',
+    			'map_type'				=> 'ROADMAP',
+    			'zoom_level' 			=> 12,
+    			'scrollwheel'			=> 1,
+    			'map_marker'			=> 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    			'get_location_message' 	=> 'Get your current location'
+    			
+    	), $org_args );
+    	
+    	extract($args);
+
+    	if ( empty( $map_marker ) ) $map_marker = 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+    	$userAddress  = false;
+    	$current_user = false;
+    	$location  	  = false;
+    	$location 	 .= '';
+    	$location 	 .= '<div id="gmw-cl-wrapper-'.$scid.'" class="gmw-cl-wrapper">';
+
+    	if ( $show_name == 1 ) {
+    	
+    		if ( is_user_logged_in() ) {
+    			global $current_user;
+    			get_currentuserinfo();
+    			$hMessage = $user_message.' '.$current_user->user_login.'!';
+    		} else {
+    			$hMessage = $guest_message;
+    		}
+    	
+    		$location .= '<div class="gmw-cl-welcome-message">'.$hMessage.'</div>';
+    	}
+    	
+    	if ( !empty( $_COOKIE['gmw_lat'] ) && !empty( $_COOKIE['gmw_lng'] ) ) {
+    	
+    		$userAddress   = array();
+    	
+    		foreach ( explode( ',', $display_by ) as $field ) {
+    			if ( isset( $_COOKIE['gmw_' . $field] ) ) {
+    				$userAddress[] = urldecode($_COOKIE['gmw_' . $field]);
+    			}
+    		}
+    	
+    		$location .= '<div class="gmw-cl-location-title-wrapper">';
+    		if ( isset( $title ) && !empty( $title ) ) {
+    			$location .= '<span class="gmw-cl-title">'.$title.'</span>';
+    		}
+    	
+    		$location .= '<span class="gmw-cl-location"><a href="#" class="gmw-cl-form-trigger" title="' . __( 'Your Current Location', 'GMW' ) . '">'.implode(' ', $userAddress) . '</a></span>';
+    		$location .= '</div>';
+    		
+    		if ( $map == 1 ) {
+    			
+    			$latitude  = urldecode( $_COOKIE['gmw_lat'] );
+    			$longitude = urldecode( $_COOKIE['gmw_lng'] );
+    			
+    			$location .= '';
+    			$location .= '<div class="gmw-cl-map-wrapper" style="width:'.$map_width.'; height:'.$map_height.'">';
+    			$location .= 	'<div id="gmw-cl-map-'.$scid.'" class="gmw-cl-map-wrapper gmw-map" style="width:100%; height:100%;"></div>';
+    			$location .= '</div>';
+    		}   		 
+    	} else { 	
+    		//disable map since we dont have location	
+    		$map = false;
+    		
+    		$location .= '<span class="gmw-cl-title"><a href="#" class="gmw-cl-form-trigger" title="'.$get_location_message.'">';
+    		$location .= $get_location_message;
+    		$location .= '</a></span>';
+    	}
+    	?>
+        <script>
+            jQuery(document).ready(function($) {
+                if ( '<?php echo $map; ?>' == 1 ) {
+                	var userLoc  = new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>);
+                    var gmwClMap = new google.maps.Map(document.getElementById('gmw-cl-map-<?php echo $scid; ?>'), {
+                        zoom: parseInt(<?php echo $zoom_level; ?>),
+                        center: userLoc,
+                        mapTypeId: google.maps.MapTypeId['<?php echo $map_type; ?>'],
+                        scrollwheel:'<?php echo $scrollwheel; ?>',
+                        mapTypeControlOptions: {
+                            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+                        }
+                    });
+     					
+                    gmwClMarker = new google.maps.Marker({
+                        position: userLoc,
+                        map: gmwClMap,
+                        icon:'<?php echo $map_marker; ?>'
+                    });            
+                };
+            });
+        </script>
+        <?php 
+    	//if only text we need
+    	if ( $text_only == 1 ) {		
+    		if ( !empty( $userAddress ) ) {
+    			return apply_filters( 'gmw_cl_display_text_only', '<span class="gmw-cl-location">'.implode(' ', $userAddress).'</span>', $userAddress, $current_user );
+    		} else {
+    			return false;
+    		}
+    	}
+    	
+    	if ( !wp_script_is( 'gmw-cl-js', 'enqueue' ) ) {
+    		wp_enqueue_script( 'gmw-cl-js' );
+    	}
+		//wp_localize_script( 'gmw-cl-js', 'gmwSettings', get_option( 'gmw_options' ) );
+    	$location .= '</div>';
+
+    	return apply_filters( 'gmw_cl_display_widget', $location, $userAddress, $display_by, $title, $show_name );
+
+    }
+
+    public function hidden_form() {
+
+        $form = '<div id="gmw-cl-hidden-form-wrapper">
+                    <form id="gmw-cl-hidden-form" method="post">
+                        <input type="hidden" id="gmw-cl-street" 		   name="gmw_cl_location[street]" value="" />
+                        <input type="hidden" id="gmw-cl-city"   		   name="gmw_cl_location[city]" value="" />
+                        <input type="hidden" id="gmw-cl-state" 			   name="gmw_cl_location[state]" value="" />
+                        <input type="hidden" id="gmw-cl-state-long" 	   name="gmw_cl_location[state_long]" value="" />
+                        <input type="hidden" id="gmw-cl-zipcode" 		   name="gmw_cl_location[zipcode]" value="" />
+                        <input type="hidden" id="gmw-cl-country" 		   name="gmw_cl_location[country]" value="" />
+                        <input type="hidden" id="gmw-cl-country-long" 	   name="gmw_cl_location[country_long]" value="" />
+                        <input type="hidden" id="gmw-cl-org-address" 	   name="gmw_cl_location[address]" value="" />
+                        <input type="hidden" id="gmw-cl-formatted-address" name="gmw_cl_location[formatted_address]" value="" />
+                        <input type="hidden" id="gmw-cl-lat" 			   name="gmw_cl_location[lat]" value="" />
+                        <input type="hidden" id="gmw-cl-lng" 			   name="gmw_cl_location[lng]" value="" />
+                        <input type="hidden" id="gmw-cl-action" 		   name="gmw_cl_action" value="post" />
+                    </form>
+                </div>';
+
+        return $form;
+
+    }
+
+    /**
+     * Current location form
+     */
+    public function cl_template() {
+
+    	$template  = '';
+    	$template .= '<div id="gmw-cl-form-wrapper" class="gmw-cl-form-wrapper">';
+    	$template .= 	'<span id="gmw-cl-close-btn">X</span>';
+    	$template .= 	'<form id="gmw-cl-form" name="gmw_cl_form" onsubmit="return false">';
+    	$template .= 		'<div id="gmw-cl-info-wrapper">';
+    	$template .= 			'<div id="gmw-cl-location-message">' . __('- Enter Your Location -', 'GMW') . '</div>';
+    	$template .= 			'<div id="gmw-cl-input-fields"><input type="text" name="gmw-cl_address" id="gmw-cl-address" value="" placeholder="zipcode or full address..." /><input id="gmw-cl-submit-address" type="submit" value="go" /></div>';
+    	$template .= 			'<div> - or - </div>';
+    	$template .= 			'<div id="gmw-cl-get-location"><a href="#" id="gmw-cl-trigger" >';
+    	$template .= 				__('Get your current location', 'GMW');
+    	$template .= 			'</a></div>';
+    	$template .= 		'</div>';
+    	$template .=		'<div id="gmw-cl-respond-wrapper" style="display:none;">';
+    	$template .= 			'<div id="gmw-cl-spinner"><img src="'.GMW_IMAGES.'/gmw-loader.gif" /></div>';
+    	$template .= 			'<div id="gmw-cl-message"></div>';
+    	$template .= 			'<div id="gmw-cl-map" style="width:100%;height:100px;display:none;"></div>';
+    	$template .=		'</div>';
+    	$template .= 	'</form>';
+    	$template .= '</div>';
+    	 
+    	$template = apply_filters( 'gmw_current_location_form', $template );
+
+    	$template .= $this->hidden_form();
+
+    	echo $template;
+    }
+
+    /**
+     * Submit user current location
+     * @param unknown_type $location
+     */
+    public function submitted_location( $location ) {
+
+        if ( empty( $_POST['gmw_cl_action'] ) )
+        	return;
+
+        //do something with the information
+        do_action( 'gmw_user_current_location_submitted', $_POST['gmw_cl_location'], get_current_user_id() );	
+        
+        //reload page to prevent form resubmission
+        wp_redirect( $_SERVER['REQUEST_URI'] );
+        exit;
+    }
+}
+new GMW_Current_Location;
